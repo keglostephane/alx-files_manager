a781f61081bef26cb873039b5ad97ffb415067a5
Keglo Dodzi <stephanekeglo@gmail.com>
2024-07-25 13:50:32 +0000

    Fix #3

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -127,22 +127,19 @@ const FilesController = {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    const { parentId = 0, page = 0 } = req.query;
-    const limit = 20;
-    const skip = parseInt(page, 10) * limit;
+    let { parentId = '0', page = '0' } = req.query;
+    page = parseInt(page, 10);
+    if (page.isNaN || page < 0) page = 0;
+    parentId = parentId === '0' ? '0' : new ObjectId(parentId);
+    const pageSize = 20;
 
-    try {
-      const files = await dbClient.db.collection('files')
-        .find({ userId: new ObjectId(userId), parentId: new ObjectId(parentId) })
-        .skip(skip)
-        .limit(limit)
-        .toArray();
+    const files = await dbClient.db.collection('files').aggregate([
+      { $match: { userId: new ObjectId(userId), parentId } },
+      { $skip: page * pageSize },
+      { $limit: pageSize },
+    ]).toArray();
 
-      return res.status(200).json(files);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
+    return res.status(200).json(files);
   },
 };
 
